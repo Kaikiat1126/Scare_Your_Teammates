b6599b2e8df680343ff239bec19a58bd559b6901
KaiKiat <92348692+Kaikiat1126@users.noreply.github.com>
2023-01-06 03:21:22 +0800

    fix: nav shape

--- src/navigation.js
+++ src/navigation.js
@@ -9,7 +9,11 @@ export default class Navigation {
     init(){
         this.contents = 
         `<div class="navbar fixed-top navpadding ">
-                <div class="nav_side"></div>
+                <div class="nav_side margin-3">
+                    <button class="btn btn-light bg-gray btn-side" type="button">
+                        <i class="fa-sharp fa-solid fa-bars icon"></i>
+                    </button>
+                </div>
                 <div class="nav_logo">
                     <img src="/APU_logo.jpeg" class="apuLogo" alt="APU logo" />
                 </div>
